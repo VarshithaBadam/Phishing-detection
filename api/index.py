96ed06f183ae5f1e079b71cf44a7f79e838c2eb5
VarshithaBadam <varshithabadam@gmail.com>
2026-03-15 20:37:48 +0530

    fix vercel deployment

--- api/index.py
+++ api/index.py
@@ -1,9 +1,9 @@
-from mangum import Mangum
 import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
+from mangum import Mangum
 from MILESTONE_2.phishgaurd.app.main import app
 
 handler = Mangum(app)
